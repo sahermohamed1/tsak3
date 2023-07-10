4a51a792032d7be43ea18866c3e0a1a30c63357f
sahermohamed1 <139110572+sahermohamed1@users.noreply.github.com>
2023-07-10 10:54:28 +0300

    Create task3.5.php

---
+++ task3.5.php
@@ -0,0 +1,8 @@
+<?php
+
+$users = [
+    'user1' => ['name' => 'ahmed', 'age' => 20, 'clr' => 'red'],
+    'user2' => ['name' => 'mohamed', 'age' => 21, 'clr' => 'green'],
+    'user3' => ['name' => 'islam', 'age' => 22, 'clr' => 'blue'],
+    'user4' => ['name' => 'abdallah', 'age' => 23, 'clr' => 'yellow'],
+];
