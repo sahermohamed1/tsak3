8125f998507655507abba6a1a74abc946d8704d4
sahermohamed1 <139110572+sahermohamed1@users.noreply.github.com>
2023-07-10 10:52:45 +0300

    Create task3.2.php

---
+++ task3.2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+</head>
+<style>
+    body {
+        font-family: 'Raleway', sans-serif;
+    }
+</style>
+
+<body>
+    <nav>
+        <a href="home.php">Home</a>|
+        <a href="about.php">about</a>|
+        <a href="contact.php">contact</a>
+    </nav>
